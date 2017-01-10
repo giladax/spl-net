dee1ef83134d7500cacaf616a861f3fa12a0ec35
Gilad <giladkoch1@gmail.com>
2017-01-10 13:49:33 +0200

    Great Success!!

---
+++ src/main/java/bgu/spl171/net/srv/ConnectionHandler.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bgu.spl171.net.srv.bidi;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ *
+ * @author bennyl
+ */
+public interface ConnectionHandler<T> extends Closeable{
+
+    void send(T msg) ; // Added this function, as per https://www.cs.bgu.ac.il/~spl171/index.php?page=Assignments.Assignment_3
+
+}
---
+++ src/main/java/bgu/spl171/net/srv/ConnectionsImpl.java
@@ -0,0 +1,35 @@
+package bgu.spl171.net.srv;
+
+import bgu.spl171.net.api.bidi.Connections;
+
+/**
+ * Created by dorgreen on 09/01/2017.
+ *
+ * This class is OUR implementation of Connections<T>.
+ *
+ * From the PDF:
+ **  Implement this class to hold a list of the new ConnectionHandler interface for each active client.
+ **  Use it to implement the interface functions.
+ **  Notice that given a connections implementation, any protocol should run.
+ **  This means that you keep your implementation of Connections on T.
+ */
+public class ConnectionsImpl<T> implements Connections<T> {
+
+    // private ConcurrentHashMap<ConnectionHandler>() // Is that a good choice of data structure? O(1) seek time + ability to remove
+
+    @Override
+    public void broadcast(T msg) {
+        // Should be something like: for(Connection C : connections) C.send(msg);
+    }
+
+    @Override
+    public boolean send(int connectionId, T msg) {
+        // Should be something like:  get(connecionId).sent(msg);
+        return false;
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+        // Should use the close() promised by Closable interface
+    }
+}
