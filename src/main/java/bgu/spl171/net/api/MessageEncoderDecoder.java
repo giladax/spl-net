3870cf89d7e266d8c09fdd5b6cb0f1e74ab41e09
Dor Green <dor@cdi-negev.com>
2017-01-10 14:12:58 +0200

    empty

--- src/main/java/bgu/spl171/net/api/MessageEncoderDecoder.java
+++ src/main/java/bgu/spl171/net/api/MessageEncoderDecoder.java
@@ -7,7 +7,7 @@ public interface MessageEncoderDecoder<T> {
      *
      * @param nextByte the next byte to consider for the currently decoded
      * message
-     * @return a message if this byte completes one or null if it doesn’t.
+     * @return a message if this byte completes one or null if it doesnt.
      */
     T decodeNextByte(byte nextByte);
 
